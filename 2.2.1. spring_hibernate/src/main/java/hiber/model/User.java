3a148fe212c8dee8fc30c872b85d9a65ad805130
pahan <https://github.com/aristovp/PP_1>
2022-02-18 01:20:26 +0300

    upd 1

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -6,6 +6,9 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
+   @OneToOne (optional=false, cascade=CascadeType.ALL)
+   @JoinColumn (name="car_id")
+   private Car car;
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
@@ -19,20 +22,20 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   public Car getCar() {return car;}
+   public void setCar(Car car) {this.car = car;}
+
    public User() {}
    
-   public User(String firstName, String lastName, String email) {
+   public User(String firstName, String lastName, String email, Car car) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.car = car;
    }
 
    public Long getId() {
       return id;
-   }
-
-   public void setId(Long id) {
-      this.id = id;
    }
 
    public String getFirstName() {
